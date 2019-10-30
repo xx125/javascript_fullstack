48b1580970d8f12e0504dbbae9338e544459ff6e
997982590@qq.com <997982590@qq.com>
2019-10-30 17:08:09 +0800

    vue第一次Demo

---
+++ vue/vue-router/src/components/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mine">detail
+    <p>{{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    created() {
+      console.log(this.$route.params.id)
+    }
+  }
+</script>
+
+<style >   
+</style>
---
+++ vue/vue-router/src/components/Mine2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mine-sec">子页面</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>   
+</style>
