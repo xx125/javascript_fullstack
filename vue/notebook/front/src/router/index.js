476c890d041ddc88e28c9d9a98670140965b5250
997982590@qq.com <997982590@qq.com>
2019-12-02 19:07:18 +0800

    前后端连接

--- vue/notebook/front/src/router/index.js
+++ vue/notebook/front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import StarNotes from '@/components/StarNotes'
+import StarBanner from '@/components/StarBanner/StarBanner'
 
 Vue.use(Router)
 
@@ -8,8 +9,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'StarNotes',
+      component: StarNotes,
+      // 配置页面的title
+      meta: {
+        title: '星辰笔记'
+      }
+    },
+    {
+      path: '/StarBanner',
+      name: 'StarBanner',
+      component: StarBanner,
+      meta: {
+        title: '欢迎'
+      }
     }
   ]
 })
